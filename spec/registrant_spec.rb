a55081013f9cb4dc10fb7a2f75aac6184f07e667
Paul Knapp <ppankluap@gmail.com>
2024-08-30 09:23:57 -0500

    feat: add test for earning a permit

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -2,17 +2,25 @@ require "spec_helper"
 
 Rspec.describe Registrant do
     before(:each) do 
-        @registrant_1 = Registrant.new('Bruce', 18, true)
-        @registrant_2 = Registrant.new('Penny', 15 )
+      @registrant_1 = Registrant.new('Bruce', 18, true)
+      @registrant_2 = Registrant.new('Penny', 15 )
     end
 
     describe '#initialize' do
-        it 'can initialize' do
-            expect(@registrant_1).to be_an_instance_of(Registrant)
-            expect(@registrant_1.name).to eq("Bruce")
-            expect(@registrant_1.age).to eq(18)
-            expect(@registrant_1.permit).to eq(true)
-            expect(@registrant_2.permit).to eq(false)
+      it 'can initialize' do
+          expect(@registrant_1).to be_an_instance_of(Registrant)
+          expect(@registrant_1.name).to eq("Bruce")
+          expect(@registrant_1.age).to eq(18)
+          expect(@registrant_1.permit).to eq(true)
+          expect(@registrant_2.permit).to eq(false)
         end   
+    end
+
+    describe '#add permit' do
+      it 'can add a permit' do
+        expect(@registrant_2.permit).to be(false)
+        @registrant_2.earn_permit
+        expect(@registrant_2.permit).to be(true)
+      end
     end
 end
