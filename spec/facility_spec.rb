1552ac3946f5777bf6e316bb7c034b6e50edbdeb
Paul Knapp <ppankluap@gmail.com>
2024-08-31 13:56:38 -0500

    feat: register vehicle collects fees

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -3,6 +3,9 @@ require 'spec_helper'
 RSpec.describe Facility do
   before(:each) do
     @facility = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+    @facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
+    @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+    @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
   end
 
   describe '#initialize' do
@@ -13,6 +16,7 @@ RSpec.describe Facility do
       expect(@facility.phone).to eq('(720) 865-4600')
       expect(@facility.services).to eq([])
       expect(@facility.collected_fees).to eq (0)
+      expect(@facility.registered_vehicles).to eq([])
     end
   end
 
@@ -23,6 +27,42 @@ RSpec.describe Facility do
       @facility.add_service('Renew Drivers License')
       @facility.add_service('Vehicle Registration')
       expect(@facility.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
+    end
+  end
+
+  describe '#can perform services' do 
+    it 'can perform services it offers' do
+      @facility.register_vehicle(@cruz)
+      expect(@facility.registered_vehicles).to contain_exactly(@cruz)
+      expect(@cruz.registration_date).to be_an_instance_of(Date)
+      expect(@cruz.plate_type).to be(:regular)
+    end
+  end
+
+  describe '#can collect fees' do 
+    it 'can collect fees' do 
+      @facility.register_vehicle(@cruz)
+      expect(@facility.collected_fees).to eq(100)
+      @facility.register_vehicle(@bolt)
+      expect(@facility.collected_fees).to eq(300)
+    end
+  end
+
+  describe '#cant provide services it doesnt have' do
+    it 'cant register vehicles if it doesnt offer registration' do
+      
+    end
+  end
+
+  describe '#unused facility doesnt change' do
+    it '#doesnt change at all' do
+      expect(@facility_2).to be_an_instance_of(Facility)
+      expect(@facility_2.name).to eq('DMV Northeast Branch')
+      expect(@facility_2.address).to eq('4685 Peoria Street Suite 101 Denver CO 80239')
+      expect(@facility_2.phone).to eq('(720) 865-4600')
+      expect(@facility_2.services).to eq([])
+      expect(@facility_2.collected_fees).to eq (0)
+      expect(@facility_2.registered_vehicles).to eq([])
     end
   end
 end
